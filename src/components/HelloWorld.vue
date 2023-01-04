9d0eb86a73a15f1c180b679ad826ff9741bc5ad6
tyron40 <https://github.com/tyron40>
2023-01-03 23:42:05 -0500

    heading section links hover and transition details

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,18 @@
 <template>
   <body>
     <section id="header">
-      <a href="#"><img src="assets/logo.png" class="logo" alt=""></a>
-  <div>
-    <ul id="navbar">
-      <li><a href="index.html">Home</a></li>
-      <li><a href="shop.html">Shop</a></li>
-      <li><a href="blog.html">Blog</a></li>
-      <li><a href="about.html">About</a></li>
-      <li><a href="contact.html">contact</a></li>
-      <li><a href="cart.html"><i class="fa-regular fa-bag-shopping"></i></a></li>
-    </ul>
-  </div>
+
+       <div>
+           <ul id="navbar">
+            <a href="#"><img src="assets/logo.png" class="logo" alt=""></a>
+              <li><a href="index.html">Home</a></li>
+              <li><a href="shop.html">Shop</a></li>
+              <li><a href="blog.html">Blog</a></li>
+              <li><a href="about.html">About</a></li>
+              <li><a href="contact.html">contact</a></li>
+              <li><a href="cart.html"><i class="fa-regular fa-bag-shopping"></i></a></li>
+          </ul>
+      </div>
        </section>
       </body>
   
@@ -28,8 +29,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+body{
+  width: 100%;
+}
+#header{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px 80px;
+  background: #E3E6F3;
+  box-shadow: 0 5px 15px rgba( 0, 0, 0, 0.6);
 }
 ul {
   list-style-type: none;
@@ -42,12 +51,5 @@ li {
 a {
   color: #42b983;
 }
-#header{
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 20px 80px;
-  background: #E3E6F3;
-  box-shadow: 0 5px 15px rgba( 0, 0, 0, 0.6);
-}
+
 </style>
